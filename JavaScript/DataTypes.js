158d763759e1a20394d8525d9f202e2f055ac541
ujunwavirginia07 <ujunwavirginia07@gmail.com>
2026-07-29 23:52:20 +0100

    DataTypes.js

--- JavaScript/DataTypes.js
+++ JavaScript/DataTypes.js
@@ -1,15 +1,2 @@
-let x
-console.log(x, typeof x);
-x = 8
-console.log(x, typeof x);
-x = x + ""
-console.log(x, typeof x);
-x = x - 2
-console.log(x, typeof x);
-x = !x 
-console.log(x, typeof x);
-
-
-console.log(Boolean(7))
-console.log(Boolean(undefined))
-console.log(Boolean("ujunwa"))
+let x = parseInt("123 Navin")
+console.log(x)
