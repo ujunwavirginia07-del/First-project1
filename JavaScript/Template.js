234d32948e04e478f9f39d7a7b6250194a839692
ujunwavirginia07 <ujunwavirginia07@gmail.com>
2026-08-01 00:17:23 +0100

    Template.js

---
+++ JavaScript/Template.js
@@ -0,0 +1,11 @@
+let num1 = 7
+let num2 = 4
+let result = num1 + num2 
+
+console.log(`The addition of ${num1} and ${num2} is ${result}`);
+
+console.log(`My Name is
+ Ujunwa`);
+
+
+//console.log("The addition of " + num1 + " add " + num2 + " is " + result)
