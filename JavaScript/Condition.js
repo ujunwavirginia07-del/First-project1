99f9c86c7f56129c4ec7462a36d1944b614a71e8
ujunwavirginia07 <ujunwavirginia07@gmail.com>
2026-07-31 23:35:13 +0100

    Condition.js

--- JavaScript/Condition.js
+++ JavaScript/Condition.js
@@ -1,16 +1,14 @@
 
 
-let num1 = 10;
-let num2 = 9;
-let num3 = 7;
+let num = 7
+let result
 
-if(num1 > num2 && num1 > num3){
-    console.log("num1 is greater")
-}
-else if(num2 > num3){
-    console.log("num2 is greater");
-}
-else{
-    console.log("num3 is greater");
-}
-console.log("bye.....");
+// if(num%2===0)
+//       result = "even"
+//else
+//      result = "odd"
+
+// ?:
+
+result = num%2===0 ? "Even" : "odd"
+console.log(result);
