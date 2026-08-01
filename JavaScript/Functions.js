ea483e3dc16988c919bab81f026ecd32859e09eb
ujunwavirginia07 <ujunwavirginia07@gmail.com>
2026-08-01 13:11:55 +0100

    Functions.js

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -1,9 +1,9 @@
-function add(num1, num2, num3)
-{
-    console.log(num1,num2, num3);
-    return num1 + num2 + num3;
-}
 
 
-let result = add(5,6,5);
-console.log(result);
+ let add = (num1,num2) => num1 + num2;
+         
+
+ let result = add(5,6)
+
+ console.log(result);
+
