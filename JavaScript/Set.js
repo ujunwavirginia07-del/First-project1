cf0d6cb60a28a31fe7e79add3ed2ddd647b75128
ujunwavirginia07 <ujunwavirginia07@gmail.com>
2026-08-02 16:21:02 +0100

    Set.js

---
+++ JavaScript/Set.js
@@ -0,0 +1,13 @@
+
+let nums = new Set();
+
+nums.add(3);
+nums.add(4);
+nums.add(3);
+nums.add("Ujunwa");
+nums.add("kiran");
+nums.add('Chiran');
+
+nums.forEach(value => {
+    console.log(value);
+});
